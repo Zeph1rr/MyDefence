31b16952fd6c4e47a878acd04cb950c10ac5c8a4
Zeph1rr <65974611+Zeph1rr@users.noreply.github.com>
2021-05-17 20:16:21 +0300

    Delete MyDefence_plan.bat

--- MyDefenceBackups/MyDefence_plan.bat
+++
@@ -1,5 +0,0 @@
-CLS
-ECHO OFF
-CHCP 1251
-SCHTASKS /Create /RU SYSTEM /SC WEEKLY /TN "DefenceBackup" /TR "C:\OpenServer\domains\test\MyDefenceBackups\MyDefence.bat" /ST 20:05:00 
-IF NOT %ERRORLEVEL%==0 MSG * "error"
